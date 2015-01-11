803c4a12343dcd4bb7749e4c494f988cd05bacfc
Ted Darling <ted@digitalspirittech.com>
2015-01-11 12:12:36 -0600

    Move the check for existing files to it's own function.

--- run_analysis.R
+++ run_analysis.R
@@ -5,10 +5,16 @@ require(dplyr)
 
 ## The data is found in the test and train folders when downloaded.
 run_analysis <- function(){
-    ## Check that the test and train directories exist. 
-    ## If they don't, we need to stop as their is no data to analyse.
-    if (!file.exists("train") && ! file.exists("test")) {
-        stop("Unable to find training and test data")
-    }
+	## Check that the folders with data exists.
+	check_files()
+}
+
+check_files <- function(){
+	## Check that the test and train directories exist. 
+	## If they don't, we need to stop as their is no data to analyse.
+	if (!file.exists("train") && ! file.exists("test")) {
+		stop("Unable to find training and test data")
+	}
+
 }
 
