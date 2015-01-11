569711e73c3bfbf95baff8d8b83766b0d5502083
Ted Darling <ted@digitalspirittech.com>
2015-01-11 12:11:52 -0600

    require dplyr package

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,14 @@
 ## This file should run as long as the Samsung data is in the working directory.
+
+## Require certain packages for working with data.
+require(dplyr)
+
 ## The data is found in the test and train folders when downloaded.
 run_analysis <- function(){
-	## Check that the folders with data exists.
-	check_files()
+    ## Check that the test and train directories exist. 
+    ## If they don't, we need to stop as their is no data to analyse.
+    if (!file.exists("train") && ! file.exists("test")) {
+        stop("Unable to find training and test data")
+    }
 }
 
-check_files <- function(){
-	## Check that the test and train directories exist. 
-	## If they don't, we need to stop as their is no data to analyse.
-	if (!file.exists("train") && ! file.exists("test")) {
-		stop("Unable to find training and test data")
-	}
-
-}
