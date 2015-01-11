415b5a776b31d44fbfa227a187914044760ed742
Ted Darling <ted@digitalspirittech.com>
2015-01-11 11:58:55 -0600

    Check for "test" and "train" folders, if they don't exist, stop with error. Add the "train" and "test" folders to .gitignore

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,8 @@
 ## This file should run as long as the Samsung data is in the working directory.
 run_analysis <- function(){
-	
+	## Check that the test and train directories exist. 
+	## If they don't, we need to stop as their is no data to analyse.
+	if (!file.exists("train") && ! file.exists("test")) {
+		stop("Unable to find training and test data")
+	}
 }
